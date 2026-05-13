62e91627587493888974acf1824971ac53ae3f5e
Ivan Lukic <mail@ivanlukic.de>
2026-05-13 19:05:51 +0200

    Change Duration property from minutes to hours

--- src/schema.py
+++ src/schema.py
@@ -3,7 +3,7 @@ REQUIRED_PROPERTIES = {
     "Title": {"title": {}},
     "IMDB": {"url": {}},
     "Director/Creator": {"select": {}},
-    "Duration [min]": {"number": {"format": "number"}},
+    "Duration [h]": {"number": {"format": "number"}},
     "IMDB Rating": {"number": {"format": "number"}},
     "Description": {"rich_text": {}},
     "Genre": {"multi_select": {}},
